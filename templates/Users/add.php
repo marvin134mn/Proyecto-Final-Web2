d430b8af44b7fa904e2049a133f1d56a2d8ade8d
marvin134mn <marvincadima66@gmail.com>
2026-04-07 11:39:57 +0200

    ui: renderizado condicional del campo role en formularios de usuario

--- templates/Users/add.php
+++ templates/Users/add.php
@@ -7,25 +7,30 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <h4 class="heading"><?= __('Acciones') ?></h4>
+            <?= $this->Html->link(__('Listar Usuarios'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column column-80">
         <div class="users form content">
             <?= $this->Form->create($user) ?>
             <fieldset>
-                <legend><?= __('Add User') ?></legend>
+                <legend><?= __('Agregar Usuario') ?></legend>
                 <?php
                     echo $this->Form->control('nombre');
                     echo $this->Form->control('apellido');
                     echo $this->Form->control('telefono'); // <-- CAMPO NUEVO AGREGADO
                     echo $this->Form->control('correo');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('language');
+                    echo $this->Form->control('password', ['type' => 'password', 'label' => 'Contraseña']);
+                    echo $this->Form->control('language', [
+                'type' => 'select',
+                'options' => ['es' => 'es', 'en' => 'en'],
+                'label' => __('Idioma'),
+                'empty' => false
+            ]);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('GUARDAR USUARIO')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
--- templates/Users/edit.php
+++ templates/Users/edit.php
@@ -7,30 +7,54 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
+            <h4 class="heading"><?= __('Acciones') ?></h4>
             <?= $this->Form->postLink(
-                __('Delete'),
+                __('Borrar Usuario'),
                 ['action' => 'delete', $user->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
+                ['confirm' => __('¿Estás seguro de que deseas eliminar al usuario # {0}?', $user->id), 'class' => 'side-nav-item']
             ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Listar Usuarios'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column column-80">
         <div class="users form content">
             <?= $this->Form->create($user) ?>
             <fieldset>
-                <legend><?= __('Edit User') ?></legend>
+                <legend><?= __('Editar Usuario') ?></legend>
                 <?php
                     echo $this->Form->control('nombre');
                     echo $this->Form->control('apellido');
                     echo $this->Form->control('telefono');
                     echo $this->Form->control('correo');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('language');
+                    
+                    // Selector de Idioma
+                    echo $this->Form->control('language', [
+                        'type' => 'select',
+                        'options' => [
+                            'es' => 'es', 
+                            'en' => 'en'
+                        ],
+                        'label' => __('Idioma'),
+                        'empty' => false
+                    ]);
+
+                    // --- CAMBIO AQUÍ: Usamos tu sesión manual ---
+                    $userSesion = $this->request->getSession()->read('Auth.User');
+                    
+                    if (isset($userSesion['role']) && $userSesion['role'] === 'admin') {
+                        echo $this->Form->control('role', [
+                            'type' => 'select',
+                            'options' => [
+                                'admin' => 'Administrador',
+                                'cliente' => 'Cliente'
+                            ],
+                            'label' => __('Asignar Rol (Privilegio Admin)'),
+                            'empty' => false
+                        ]);
+                    }
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Guardar Cambios')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
